ade935a485a1592d0c76ca2418b59c92d077af4e
Uzytkownik <michaltyrala95@icloud.com>
2022-02-17 21:01:06 +0100

    commit3

--- src/main/java/bankproject/Bank.java
+++ src/main/java/bankproject/Bank.java
@@ -1,5 +1,24 @@
 package bankproject;
 
-public class Bank {
+import java.util.List;
 
+public class Bank {
+    private int bankId;
+    private List<Client> clientList;
+
+    public Bank() {
+    }
+
+    public List<Client> newClient(int clientId, String firstName, String lastName) {
+        Client client = new Client();
+        client.setClientId(clientId);
+        client.setFirstName(firstName);
+        client.setLastName(lastName);
+        clientList.add(client);
+        return clientList;
+    }
+
+    public void deleteClient(Client client){
+        clientList.remove(client);
+    }
 }
