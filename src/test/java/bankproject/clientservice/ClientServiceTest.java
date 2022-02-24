078a417028fb6f5dd1e42ed2c17da34968129c6f
Uzytkownik <michaltyrala95@icloud.com>
2022-02-24 19:55:35 +0100

    feature(): new test

--- src/test/java/bankproject/clientservice/ClientServiceTest.java
+++ src/test/java/bankproject/clientservice/ClientServiceTest.java
@@ -21,33 +21,34 @@ class ClientServiceTest {
     public void create() {
         clientService = new ClientService();
         account = new Account();
-        account.setBalance(BigDecimal.valueOf(100));
-        clientService.openAccount(account);
     }
 
     @Test
     void payInto() {
+        //given
+        account.setBalance(BigDecimal.valueOf(100));
+        clientService.openAccount(account);
+        //when
         clientService.payIntoAccount(account.getAccountId(), BigDecimal.valueOf(20));
-        Account account1 = clientService.findAccountById(account.getAccountId());
+        Account tempAccount = clientService.findAccountById(account.getAccountId());
+        //then
+        assertEquals(BigDecimal.valueOf(120).stripTrailingZeros(), tempAccount.getBalance().stripTrailingZeros());
+    }
 
-        assertEquals(BigDecimal.valueOf(120.00).stripTrailingZeros(), account1.getBalance().stripTrailingZeros());
+    @Test
+    void withdraw() {
+        //given
+        account.setBalance(BigDecimal.valueOf(100));
+        clientService.openAccount(account);
+        //when
+        clientService.withdrawFromAccount(account.getAccountId(), BigDecimal.valueOf(20));
+        Account tempAccount = clientService.findAccountById(account.getAccountId());
+        //then
+        assertEquals(BigDecimal.valueOf(80).stripTrailingZeros(), tempAccount.getBalance().stripTrailingZeros());
     }
 
     @AfterEach
     public void drop() {
         clientService.closeAccount(account.getAccountId());
-    }
-
-    @Test
-    void withdraw() {
-        ClientService clientService = new ClientService();
-        Account account = new Account();
-        account.setBalance(BigDecimal.valueOf(100));
-        clientService.openAccount(account);
-
-        clientService.withdrawFromAccount(account.getAccountId(), BigDecimal.valueOf(20));
-        BigDecimal balance = account.getBalance();
-
-        assertEquals(BigDecimal.valueOf(80), balance);
     }
 }
